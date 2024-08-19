78192828c9f3578032dddaf5d7b6f6f0541b0838
mkarthikeyan31 <karthikeyan311994@gmail.com>
2024-08-19 13:12:31 +0530

    updated behaviour structure

--- lib/matplotex/blueprint/chart.ex
+++ lib/matplotex/blueprint/chart.ex
@@ -1,71 +1,48 @@
 defmodule Matplotex.Blueprint.Chart do
   alias Matplotex.Blueprint.Label
   alias Matplotex.Blueprint.Line
-  @type dataset() :: %{x: list(), y: list()}
-  @type labels() :: [Label.t()] | map()
-  @type lines() :: [Line.t()]
-  @type word() :: String.t()
-  @type label_prefix() :: %{x: String.t(), y: String.t()}
-  @type label_offset() :: %{x: number(), y: number()}
-  @type chart_params() :: %{
-          color_palette: list(),
-          dataset: dataset(),
-          height: number(),
-          labels: map(),
-          label_offset: number(),
-          label_prefix: map(),
-          margin: number(),
-          type: module(),
-          width: number(),
-          y_max: number(),
-          y_scale: number()
-        }
+  @type dataset1D() :: %{x: list()}
+  @type dataset2D() :: %{x: list(), y: list()}
+  @type font() :: %{size: String.t(), weight: String.t(), color: String.t()}
+  @type label() :: %{x: String.t() | nil, y: String.t() | nil, font: font() | nil}
+  @type scale() :: %{x: number() | nil, y: number() | nil, aspect_ratio: number | nil}
+  @type grid() :: %{x_scale: number() | nil, y_scale: number() | nil} | nil
+  @type title() :: %{title: String.t() | nil, font_size: font() | nil}
+  @type size() :: %{height: number(), width: number()} | nil
+  @type tick() :: %{x_scale: number() | nil, y_scale: number() | nil, font: font()}
+  @type margin() :: %{x_margin: number(), y_margin: number()} | nil
+  @type valid?() :: :ok | {:error, String.t()}
+  @type axis() :: :on | :off
+  @type element() :: any()
+
   @type t() :: %__MODULE__{
-          axis_lines: list(),
-          content: any(),
-          color_palette: list(),
-          dataset: dataset(),
-          elements: any(),
-          errors: list(),
-          grid_lines: lines(),
-          height: float(),
-          label_offset: label_offset(),
-          label_prefix: label_prefix(),
-          labels: labels(),
-          show_axis: boolean(),
-          show_grid_lines: boolean(),
-          margin: float(),
-          type: atom(),
-          valid?: boolean(),
-          width: number(),
-          x_max: number(),
-          x_scale: number(),
-          y_max: number(),
-          y_scale: number()
+          dataset: dataset1D() | dataset2D(),
+          label: label(),
+          scale: scale(),
+          grid: grid(),
+          title: title(),
+          size: size(),
+          tick: tick(),
+          margin: margin(),
+          valid?: valid(),
+          axis: axis(),
+          element: element(),
+          type: module()
         }
 
   defstruct [
-    :axis_lines,
-    :content,
-    :color_palette,
     :dataset,
-    :elements,
-    :errors,
-    :grid_lines,
-    :height,
-    :label_offset,
-    :label_prefix,
-    :labels,
+    :label,
+    :scale,
+    :grid,
+    :title,
+    :size,
+    :tick,
     :margin,
-    :show_axis,
-    :show_grid_lines,
-    :type,
     :valid?,
-    :width,
-    :x_max,
-    :x_scale,
-    :y_max,
-    :y_scale
+    :axis,
+    :element,
+    :type
   ]
 
   @spec new(chart_params()) :: __MODULE__.t()
